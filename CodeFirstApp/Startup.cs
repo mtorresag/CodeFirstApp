fbac521d734b274ebd9f00b79c02ac8d0c454f4e
martin.torres <martin.torres@APCLOTIAN04>
2024-04-01 09:10:22 -0500

    Agregar archivos de proyecto.

---
+++ CodeFirstApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeFirstApp.Startup))]
+namespace CodeFirstApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
